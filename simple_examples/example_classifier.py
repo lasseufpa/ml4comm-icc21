7483d086d0b90b2e33f0d496cf15fcb9da4b575f
1176325 <1176325@gitlab.lasse.ufpa.br>
2021-06-10 12:24:20 -0300

    Cleaned up the code

--- simple_examples/example_classifier.py
+++ simple_examples/example_classifier.py
@@ -1,13 +1,10 @@
-import numpy as np
-from numpy.random import randint, randn, random, standard_normal
+from numpy.random import randint, standard_normal
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 #basic problem definitions
@@ -17,21 +14,20 @@ num_train_examples = 10 # number of training examples
 
 #generate some random data
 X_train = standard_normal((num_train_examples, num_features))
-y_train = np.random.randint(num_classes, size=num_train_examples)
+y_train = randint(num_classes, size=num_train_examples)
 X_test = standard_normal((num_train_examples, num_features))
-y_test = np.random.randint(num_classes, size=num_train_examples)
+y_test = randint(num_classes, size=num_train_examples)
 
-#train classifiers
-#classifier = LinearSVC() #linear SVM (maximum margin perceptron)
-#classifier = MLPClassifier(alpha=0.1, max_iter=500)
-#classifier = KNeighborsClassifier(3)
-
+#train classifiers - choose one
+classifier = LinearSVC() #linear SVM (maximum margin perceptron)
+classifier = MLPClassifier(alpha=0.1, max_iter=500)
+classifier = KNeighborsClassifier(3)
 classifier = DecisionTreeClassifier(max_depth=20) #single tee
 classifier = RandomForestClassifier(n_estimators=30,max_depth=10) #30 trees
 classifier = SVC(gamma=1, C=1) #SVM with RBF kernel
 
-classifier.fit(X_train, y_train) #train the classifier
-y_predicted = classifier.predict(X_test) #
+classifier.fit(X_train, y_train) #training stage
+y_predicted = classifier.predict(X_test) #test stage
 print('Accuracy = ', accuracy_score(y_test, y_predicted))
 
 print(X_train)
